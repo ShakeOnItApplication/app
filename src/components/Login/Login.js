ca1f23f252f50e9ac1bbc537f8def8800afad1bd
Frank Robenalt <frank.robenalt@gmail.com>
2018-01-15 21:37:46 -0500

    passport fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,14 +5,18 @@ export default class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showLogin: true,
-            showSignup: false
+            showLogin: true
         }
     }
     login(info) {
         axios.post('/auth/login', info).then(response => {
             console.log(response);
         });
+    }
+    registerUser(info) {
+        axios.post('/api/registerUser', info).then(res => {
+            console.log(res);
+        })
     }
 
     render() {
@@ -22,13 +26,27 @@ export default class Login extends Component {
                     ?
                     <div>
                         <div>Username</div>
-                        <input type="text" onChange={(e)=>this.setState({ username: e.target.value })} />
+                        <input type="text" onChange={(e) => this.setState({ username: e.target.value })} />
                         <div>Password</div>
-                        <input type="text" onChange={(e)=>this.setState({ password: e.target.value })} />
-                        <button onClick={()=>this.login({username: this.state.username, password: this.state.password})}>Log In</button>
+                        <input type="text" onChange={(e) => this.setState({ password: e.target.value })} />
+                        <button onClick={() => this.login({ username: this.state.username, password: this.state.password })}>Log In</button>
+                        <div onClick={()=> this.setState({ showLogin: false }) }>Create Account</div>
                     </div>
                     :
-                    <input type="text" />
+                    <div>
+                        <div>Username</div>
+                        <input type="text" onChange={(e) => this.setState({ username: e.target.value })} />
+                        <div>Password</div>
+                        <input type="text" onChange={(e) => this.setState({ password: e.target.value })} />
+                        <div>Email</div>
+                        <input type="text" onChange={(e) => this.setState({ email: e.target.value })} />
+                        <div>First Name</div>
+                        <input type="text" onChange={(e) => this.setState({ first_name: e.target.value })} />
+                        <div>Last Name</div>
+                        <input type="text" onChange={(e) => this.setState({ last_name: e.target.value })} />
+                        <button onClick={() => this.registerUser({ username: this.state.username, password: this.state.password, email: this.state.email, first_name: this.state.first_name, last_name: this.state.last_name })}>Register</button>
+                        <div onClick={()=> this.setState({ showLogin: true }) }>Log In</div>
+                    </div>
                 }
             </div>
         )
