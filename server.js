98f513ef67f764ae29bc88c972e5231e3a2cf4bc
Frank Robenalt <frank.robenalt@gmail.com>
2018-02-28 09:04:00 -0500

    'kk'

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./server/server');
 const port = 3030;
 
-app.listen(process.env.PORT || port, function() {
+app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
