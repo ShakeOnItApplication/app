cb3a9a095fcf962797ea450f72c00ab924a2f60d
Frank Robenalt <frank.robenalt@gmail.com>
2018-03-03 13:31:52 -0500

    'f'

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use(cookieParser());
 app.get('/*', (req, res) => {
   console.log(__dirname);
     console.log('hi from app.get')
-    res.sendFile(__dirname + "/public")
+    res.sendFile(__dirname + "/public/index.html")
   });
 
 const stripeCtrl = require("./server/stripe/stripeCtrl");
