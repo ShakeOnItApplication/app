94b9b4ad64f895576e21ccafffdf8768aa4ca758
Frank Robenalt <frank.robenalt@gmail.com>
2018-03-03 13:03:17 -0500

    'p'

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ const cookieParser = require("cookie-parser");
 const flash = require("connect-flash");
 const app = express();
 
-console.log('yo')
+app.engine('html', require('ejs').renderFile);
+
 app.use(cors());
 app.use(cookieParser());
 // this is the build command for when we host the app, npm start builds it for us, so we finna leave it commented out til we done
 
-app.use(express.static(path.resolve(__dirname, '/')));
+app.use(express.static(`${__dirname}/build`));
+
+// app.use(express.static(path.resolve(__dirname, '/')));
 // app.get('/*', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 // });
