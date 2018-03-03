4b144984fe92ca21c96c179a8ef0c4758957c95d
Frank Robenalt <frank.robenalt@gmail.com>
2018-03-03 14:07:59 -0500

    'd'

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(cors());
 app.use(cookieParser());
 // this is the build command for when we host the app, npm start builds it for us, so we finna leave it commented out til we done
-app.use(express.static('build'));
+app.use(express.static(`${__dirname}/build`));
 
 // app.use(express.static(`${__dirname}/build`));
 
